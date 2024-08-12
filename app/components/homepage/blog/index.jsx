7794387bb6e3e5ee212d18cd6381e037c53a469b
Personal-Vijayji1137 <personal.vijayji1137@gmail.com>
2024-08-13 01:44:49 +0530

    about

--- app/components/homepage/blog/index.jsx
+++ app/components/homepage/blog/index.jsx
@@ -1,4 +1,4 @@
-'use server'
+// @flow strict
 import Link from 'next/link';
 import { FaArrowRight } from 'react-icons/fa';
 import BlogCard from './blog-card';
@@ -15,6 +15,7 @@ async function getData() {
 };
 
 async function Blog() {
+  noStore();
   const blogs = await getData();
   return (
     <div id='blogs' className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
@@ -48,7 +49,7 @@ async function Blog() {
         <Link
           className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
           role="button"
-          href="https://www.blog.iplust.in/"
+          href="/blog"
         >
           <span>View More</span>
           <FaArrowRight size={16} />
